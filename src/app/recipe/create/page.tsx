2743862b62457d07197245a80203c809d005c107
Anderson Keller <andersonkellervendas@hotmail.com>
2023-07-27 23:00:26 -0300

    fix: main heigth global

--- src/app/recipe/create/page.tsx
+++ src/app/recipe/create/page.tsx
@@ -2,13 +2,15 @@ import { CreteRecipeForm } from "@/components/createRecipe/CreateForm";
 
 export default function CreateRecipe() {
   return (
-    <section className="min-h-screen h-max pb-20">
-      <h3>Vamos criar uma receita!</h3>
-      <p>
-        Tem aquela receita super bacana para compartilhar com a gente? então
-        bora cria-lá
-      </p>
-      <CreteRecipeForm />
-    </section>
+    <main>
+      <section className="pb-20">
+        <h3>Vamos criar uma receita!</h3>
+        <p>
+          Tem aquela receita super bacana para compartilhar com a gente? então
+          bora cria-lá
+        </p>
+        <CreteRecipeForm />
+      </section>
+    </main>
   );
 }
